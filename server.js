83f0a0a6cffd4b47cadcd38b97e80f25021514da
renatormsilva <renato.brtkm@gmail.com>
2020-11-13 10:39:56 -0400

    new featureds

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
+const { urlencoded } = require('express');
 const express = require('express');
 const nunjucks = require('nunjucks');
-
+const routes = require("./routes");
 const server = express();
+
+server.use(urlencoded({extended: true}))
 
 server.use(express.static('public'));
 server.use(express.static('photos'));
 
 server.set("view engine", "njk")
 
+server.use(routes)
+
 nunjucks.configure("views", {
     express: server,
     autoescape: false,
     noCache: true
     
-});
-
-server.get("/", function(req, res) {
-    return res.render("index")
 });
 
 
